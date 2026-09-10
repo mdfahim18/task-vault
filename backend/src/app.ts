6103447be285d96be28a41fe77789229cc2a66c4
Mahfuzur Rahman Shanto <mrahman7148@gmail.com>
2026-09-10 16:02:09 +0600

    feat: implement global error handling middleware with standardized response envelope structures

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import {errorMiddleware} from "@middlewares/error.middleware.js";
 
 export const api_timeout = 15_000
 export const app = express()
+
+app.use(errorMiddleware)
